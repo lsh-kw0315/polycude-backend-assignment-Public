60b3ba452ec96ab62eba338710bfdbb1886b6ad5
LSH-kw0315 <tmdgjsdl1234@naver.com>
2026-01-28 01:53:46 +0900

    GCP 인증이 서버에서 안되는 것 해결 중

--- src/main/java/prac/backendassingment/application/controller/AdminController.java
+++ src/main/java/prac/backendassingment/application/controller/AdminController.java
@@ -1,4 +1,27 @@
 package prac.backendassingment.application.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import prac.backendassingment.application.dto.ProductAddRequest;
+import prac.backendassingment.application.service.ProductService;
+import prac.backendassingment.domain.model.Post;
+import prac.backendassingment.domain.model.Product;
+
+@RestController
+@RequiredArgsConstructor
 public class AdminController {
+    private final ProductService productService;
+
+    @PostMapping("/products")
+    public Product addProduct(ProductAddRequest request){
+        return productService.addNewProduct(request);
+    }
+
+    @GetMapping("/products/{id}")
+    public Product getProducts(@PathVariable("id")Long id){
+        return productService.findProductById(id);
+    }
 }
